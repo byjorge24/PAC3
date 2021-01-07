a90697cae76e7119aa4485cd561f67f33fb582e5
Jorge <jorgeof30@gmail.com>
2021-01-07 16:04:27 +0100

    He añadido los archivos de la calculadora a mi repositorio

---
+++ Unidad4/Make/calc2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int suma(int op1, int op2){
+	return (op1+op2);
+}
+
+int resta(int op1, int op2){
+	return (op1-op2);
+}
+
+int multiplica(int op1, int op2){
+	return (op1*op2);
+}
+
+int divideix(int op1, int op2){
+	return (op1/op2);
+}
+
